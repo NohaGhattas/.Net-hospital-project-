70d42a1c91de0c2f6e7cb61be6be4ebab366fc8f
lobna <lobnaraafat23@gmail.com>
2025-04-25 03:29:08 +0300

    Add Admin Area for Dashboard

---
+++ HospitalManagementSystem/HospitalManagementSystem.Presentation/Areas/Admin/Controllers/DoctorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DoctorController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddDoctor()
+        {
+            return View();
+        }
+    }
+}
--- HospitalManagementSystem/HospitalManagementSystem.Presentation/Program.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Presentation/Program.cs
@@ -46,11 +46,17 @@ namespace HospitalManagementSystem.Presentation
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.MapControllerRoute(
+     name: "areas",
+     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.UseAuthorization();
+
+           
             app.MapRazorPages();
 
             app.Run();
