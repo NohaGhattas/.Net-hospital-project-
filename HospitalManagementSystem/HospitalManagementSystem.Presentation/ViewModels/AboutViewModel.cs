adc730c001be76aaafa15a088943043c6160fd71
Yasmin Khaled <yasmin.khaled.f@gmail.com>
2025-04-26 21:21:02 +0300

    Added About controller

---
+++ HospitalManagementSystem/HospitalManagementSystem.Presentation/ViewModels/AboutViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HospitalManagementSystem.Presentation.ViewModels
+{
+    public class AboutViewModel
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
+
